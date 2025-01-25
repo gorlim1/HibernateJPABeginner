1a12f58d214e2c870d4f36bcc3e90e224808f974
Desmond Roche <desmondroche@gmail.com>
2025-01-24 20:50:34 -0500

    Added code for this Per Scholas GLAB

--- src/main/java/com/roche/controller/UserController.java
+++ src/main/java/com/roche/controller/UserController.java
@@ -2,10 +2,13 @@ package com.roche.controller;
 
 
 import com.roche.model.User;
+import jakarta.persistence.TypedQuery;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+
+import java.util.List;
 
 
 public class UserController {
@@ -16,11 +19,23 @@ public class UserController {
 
 //        addUser(factory, session);
 //
-        findUser(factory,session,5);
+//        findUser(factory,session,9);
 //
 //        updateUser(session,3);
 //
 //        deleteUser(session,2);
+
+//        findUserHQL(factory, session);
+
+//        getRecordById(factory, session);
+
+//        getRecords(session);
+
+//        getMaxSalary(session);
+
+//        getMaxSalaryGroupBy();
+
+        namedQueryExample(session);
 
         session.close();
         factory.close();
@@ -112,5 +127,79 @@ public class UserController {
         tx.commit();
 
         session.close();
+    }
+
+    public static void findUserHQL(SessionFactory factory, Session session) {
+
+        String hqlFrom = "FROM User"; // Example of HQL to get all records of user class
+        String hqlSelect = "SELECT u FROM User u";
+
+        TypedQuery<User> query = session.createQuery(hqlSelect, User.class);
+        List<User> results = query.getResultList();
+
+        System.out.printf("%s%13s%17s%34s%n","|User Id","|Full name","|Email","|Password");
+        for (User u : results) {
+            System.out.printf(" %-10d %-20s %-30s %s %n", u.getId(), u.getFullName(),
+                    u.getEmail(), u.getPassword());
+
+        }
+    }
+
+    public static void getRecordById(SessionFactory factory, Session session) {
+
+        String hql = "FROM User u WHERE u.id > 2 ORDER BY u.salary DESC";
+        TypedQuery<User> query = session.createQuery(hql, User.class);
+        List<User> results = query.getResultList();
+
+        System.out.printf("%s%13s%17s%34s%n","|User Id","|Full name","|Email","|Password");
+        for (User u : results) {
+            System.out.printf(" %-10d %-20s %-30s %-23s %s %n", u.getId(),
+                    u.getFullName(), u.getEmail(), u.getPassword(), u.getSalary());
+        }
+    }
+
+    public static void getRecords(Session session) {
+        TypedQuery<Object[]> query =
+                session.createQuery("SELECT U.salary, U.fullName FROM User AS U", Object[].class);
+        List<Object[]> results = query.getResultList();
+
+        System.out.printf("%s%13s%n","Salary","City");
+        for (Object[] object : results) {
+            System.out.printf("%-16s%s%n", object[0], object[1]);
+        }
+    }
+
+    public static void getMaxSalary(Session session) {
+        String hql = "SELECT max(U.salary) FROM User AS U";
+        TypedQuery<Object> query = session.createQuery(hql, Object.class);
+        Object result = query.getSingleResult();
+//        System.out.println(result);
+        System.out.printf("%s%s\n", "Maximum Salary: ", result);
+    }
+
+    public static void getMaxSalaryGroupBy () {
+        SessionFactory factory = new Configuration().configure().buildSessionFactory();
+        Session session = factory.openSession();
+        String hql = "SELECT SUM(U.salary), U.city FROM User AS U GROUP BY U.city";
+
+        TypedQuery<Object[]> query = session.createQuery(hql);
+        List<Object[]> result = query.getResultList();
+
+        for (Object[] object : result) {
+            System.out.println("Total salary " + object[0] + " | city: " + object[1]);
+        }
+    }
+
+    public static void namedQueryExample(Session session) {
+        String hql = " FROM User AS u WHERE u.id = :id";
+        TypedQuery<User> query = session.createQuery(hql, User.class);
+        query.setParameter("id", 5);
+        List<User> result = query.getResultList();
+
+        System.out.printf("%s%13s%17s%34s%21s%n", "|User Id", "|Full name", "|Email", "|Password", "|Salary");
+        for (User u : result) {
+            System.out.printf(" %-10d %-20s %-30s %-23s %s %n", u.getId(), u.getFullName(),
+                    u.getEmail(), u.getPassword(), u.getSalary());
+        }
     }
 }
